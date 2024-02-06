bfec0c966d65a7647e365022fe142649d5cde196
Frank Cairampoma <ingfralch@gmail.com>
2024-02-06 17:57:16 -0500

    Actualizar estilos del carrusel en Hello.tsx

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,73 +1,77 @@
 function Hello() {
   return (
     <div>
-      <div class="carousel">
-        <div class="carousel-inner">
+      <div className="carousel">
+        <div className="carousel-inner">
           <input
-            class="carousel-open"
+            className="carousel-open"
             type="radio"
             id="carousel-1"
             name="carousel"
             aria-hidden="true"
-            hidden=""
-            checked="checked"
+            hidden={false}
+            checked={true}
           />
-          <div class="carousel-item">
-            <img src="http://fakeimg.pl/2000x800/0079D8/fff/?text=Without" />
+          <div className="carousel-item" style={{ width: "100%", height: "800px", overflow: "hidden" }}>
+          <img 
+              src="https://universidadeuropea.com/resources/media/images/Tipos_de_abogados_og.original.jpg" 
+              style={{ width: "100%", height: "100%", objectFit: "cover" }}
+              alt="Descripción de la imagen"
+            />
           </div>
           <input
-            class="carousel-open"
+            className="carousel-open"
             type="radio"
             id="carousel-2"
             name="carousel"
             aria-hidden="true"
-            hidden=""
+            hidden={false}
           />
-          <div class="carousel-item">
+          <div className="carousel-item">
             <img src="http://fakeimg.pl/2000x800/DA5930/fff/?text=JavaScript" />
           </div>
           <input
-            class="carousel-open"
+            className="carousel-open"
             type="radio"
             id="carousel-3"
             name="carousel"
             aria-hidden="true"
-            hidden=""
+            hidden={false}
           />
-          <div class="carousel-item">
+          <div className="carousel-item">
             <img src="http://fakeimg.pl/2000x800/F90/fff/?text=Carousel" />
           </div>
-          <label for="carousel-3" class="carousel-control prev control-1">
+          <label htmlFor="carousel-3" className="carousel-control prev control-1">
             ‹
           </label>
-          <label for="carousel-2" class="carousel-control next control-1">
+          <label htmlFor="carousel-2" className="carousel-control next control-1">
             ›
           </label>
-          <label for="carousel-1" class="carousel-control prev control-2">
+          <label htmlFor="carousel-1" className="carousel-control prev control-2">
             ‹
           </label>
-          <label for="carousel-3" class="carousel-control next control-2">
+          <label htmlFor="carousel-3" className="carousel-control next control-2">
             ›
           </label>
-          <label for="carousel-2" class="carousel-control prev control-3">
+          <label htmlFor="carousel-2" className="carousel-control prev control-3">
             ‹
           </label>
-          <label for="carousel-1" class="carousel-control next control-3">
+          <label htmlFor="carousel-1" className="carousel-control next control-3">
             ›
           </label>
-          <ol class="carousel-indicators">
+          <ol className="carousel-indicators">
             <li>
-              <label for="carousel-1" class="carousel-bullet">
+              <label htmlFor="carousel-1" className="carousel-bullet">
                 •
               </label>
             </li>
             <li>
-              <label for="carousel-2" class="carousel-bullet">
+              <label htmlFor="carousel-2" className="carousel-bullet">
                 •
               </label>
             </li>
             <li>
-              <label for="carousel-3" class="carousel-bullet">
+              <label htmlFor="carousel-3" className="carousel-bullet">
                 •
               </label>
             </li>
