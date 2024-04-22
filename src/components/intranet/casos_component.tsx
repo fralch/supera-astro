66b799294262185281b5d4c74cf8f9da4293d3dc
FrankCairampoma <ingfralch@gmail.com>
2024-04-22 16:40:52 -0500

    Actualiza estilos de lista en la página de Servicios de la página de inicio

---
+++ src/components/intranet/casos_component.tsx
@@ -0,0 +1,172 @@
+
+const Casos_component = () => {
+    return (
+        <section className="  py-2 bg-blueGray-50">
+            <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4 mx-auto mt-24">
+                <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded ">
+                    <div className="rounded-t mb-0 px-4 py-3 border-0">
+                        <div className="flex flex-wrap items-center">
+                            <div className="relative w-full max-w-full flex-grow flex-1">
+                                <h2 className="font-semibold text-xl text-blueGray-700">
+                                    Casos de Supera Abogados
+                                </h2>
+                            </div>
+                            
+
+                        </div>
+                    </div>
+
+                    <div className="block w-full overflow-x-auto">
+                        <table className="items-center bg-transparent w-full border-collapse text-slate-600">
+                            <thead>
+                                <tr>
+                                    <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                                        Nombre
+                                    </th>
+                                    <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                                        Área
+                                    </th>
+                                    <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                                        Ultima actualización
+                                    </th>
+                                    <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                                        Avance
+                                    </th>
+                                    <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                                        Estado
+                                    </th>
+                                </tr>
+                            </thead>
+
+                            <tbody>
+                                <tr>
+                                    <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left text-blueGray-700 ">
+                                        /argon/
+                                    </th>
+                                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 ">
+                                        Penal
+                                    </td>
+                                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 ">
+                                        10/10/2021 - 13:00
+                                    </td>
+                                    <td className="border-t-0 px-6 align-center border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                                        <div className="flex items-center">
+                                            <span className="mr-2 text-xs font-medium  text-gray-500">39%</span>
+                                            <div className="relative w-3/5">
+                                                <div className="w-full bg-gray-200 rounded-sm h-2">
+                                                    <div className="bg-indigo-600 h-2 rounded-sm" style={{ width: '39%' }}></div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                                        <span className="bg-green-500 text-white py-1 px-2 rounded-full text-xs">Active</span>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left text-blueGray-700">
+                                        /argon/index.html
+                                    </th>
+                                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                                        Civil
+                                    </td>
+                                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                                        02/06/2021 - 08:12
+                                    </td>
+                                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                                        <div className="flex items-center">
+                                            <span className="mr-2 text-xs font-medium  text-gray-500">9%</span>
+                                            <div className="relative w-3/5">
+                                                <div className="w-full bg-gray-200 rounded-sm h-2">
+                                                    <div className="bg-red-600 h-2 rounded-sm" style={{ width: '9%' }}></div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                                        <span className="bg-green-500 text-white py-1 px-2 rounded-full text-xs">Active</span>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left text-blueGray-700">
+                                        /argon/charts.html
+                                    </th>
+                                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                                        Comunidades
+                                    </td>
+                                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                                        02/06/2021 - 08:12
+                                    </td>
+                                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                                        <div className="flex items-center">
+                                            <span className="mr-2 text-xs font-medium  text-gray-500">9%</span>
+                                            <div className="relative w-3/5">
+                                                <div className="w-full bg-gray-200 rounded-sm h-2">
+                                                    <div className="bg-red-600 h-2 rounded-sm" style={{ width: '9%' }}></div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                                        <span className="bg-red-500 text-white py-1 px-2 rounded-full text-xs">Inactive</span>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left text-blueGray-700">
+                                        /argon/tables.html
+                                    </th>
+                                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                                        Laboral
+                                    </td>
+                                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                                        02/06/2021 - 08:12
+                                    </td>
+                                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                                        <div className="flex items-center">
+                                            <span className="mr-2 text-xs font-medium  text-gray-500">9%</span>
+                                            <div className="relative w-3/5">
+                                                <div className="w-full bg-gray-200 rounded-sm h-2">
+                                                    <div className="bg-red-600 h-2 rounded-sm" style={{ width: '9%' }}></div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                                        <span className="bg-green-500 text-white py-1 px-2 rounded-full text-xs">Active</span>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left text-blueGray-700">
+                                        /argon/profile.html
+                                    </th>
+                                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                                        Corporativo
+                                    </td>
+                                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                                        02/06/2021 - 08:12
+                                    </td>
+                                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                                        <div className="flex items-center">
+                                            <span className="mr-2 text-xs font-medium  text-gray-500">9%</span>
+                                            <div className="relative w-3/5">
+                                                <div className="w-full bg-gray-200 rounded-sm h-2">
+                                                    <div className="bg-red-600 h-2 rounded-sm" style={{ width: '9%' }}></div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                                        <span className="bg-green-500 text-white py-1 px-2 rounded-full text-xs">Active</span>
+                                    </td>
+                                </tr>
+                            </tbody>
+
+                        </table>
+                    </div>
+                </div>
+            </div>
+
+        </section>
+    )
+}
+export default Casos_component;
