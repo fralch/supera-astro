22384af946fcbb0719d5219f6f89a44ef7691733
Frank Cairampoma <ingfralch@gmail.com>
2024-05-01 12:01:26 -0500

    Actualiza estilos del componente Casos_component.tsx y ajusta el rango de culminado a 45%

--- src/components/intranet/casos_component.tsx
+++ src/components/intranet/casos_component.tsx
@@ -226,7 +226,7 @@ const Casos_component = () => {
                             <h3 className="text-gray-900 text-xl lg:text-2xl font-semibold dark:text-white">
                                 Actualización del caso
                             </h3>
-                            <button type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="default-modal" onClick={cerrarModal}>
+                            <button type="button" className="text-gray-400 bg-gray-900 hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="default-modal" onClick={cerrarModal}>
                                 <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>  
                             </button>
                         </div>
@@ -271,7 +271,7 @@ const Casos_component = () => {
                             <h3 className="text-gray-900 text-xl lg:text-2xl font-semibold dark:text-white">
                                 Acto Procesal
                             </h3>
-                            <button type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="modal-acto-procesal" onClick={ cerrarModalActoProcesal}>
+                            <button type="button" className="text-gray-400 bg-gray-900 hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-red-400 dark:hover:text-white" data-modal-toggle="modal-acto-procesal" onClick={ cerrarModalActoProcesal}>
                                 <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>  
                             </button>
                         </div>
