e627a2c1e88a70fcef7561ab64ef0467a98a837f
FrankCairampoma <ingfralch@gmail.com>
2024-05-07 18:34:56 -0500

    Agrega funcionalidad de inicio de sesión con tecla Enter en el componente Login.tsx

--- src/components/intranet/casos_component.tsx
+++ src/components/intranet/casos_component.tsx
@@ -4,6 +4,8 @@ import axios from 'axios';
 
 const Casos_component = () => {
     const [dateTime, setDateTime] = useState(new Date());
+    const [time, setTime] = useState(0);
+    const [isRunning, setIsRunning] = useState(false);
     const [actoProcesal, setActoProcesal] = useState('');
     const [culminado, setCulminado] = useState(0);
     const [idCasoUpdate, setIdCasoUpdate] = useState(0);
@@ -57,6 +59,23 @@ const Casos_component = () => {
         setDataTableFilter(data);
     }, [mesaFilter]);
 
+    
+    useEffect(() => {
+        let interval;
+
+        if (isRunning) {
+        interval = setInterval(() => {
+            setTime(prevTime => prevTime + 100);
+        }, 100);
+        } else {
+        clearInterval(interval);
+        }
+
+        return () => {
+        clearInterval(interval);
+        };
+    }, [isRunning]);
+
     const abrirModal = (actoProcesal, idCasoUpdate, culminado) => {
         setActoProcesal(actoProcesal);
         setIdCasoUpdate(idCasoUpdate);
@@ -104,6 +123,14 @@ const Casos_component = () => {
             })
 
     }
+
+    const formatTime = (ms) => {
+        const seconds = Math.floor((ms / 1000) % 60);
+        const minutes = Math.floor((ms / (1000 * 60)) % 60);
+        const hours = Math.floor((ms / (1000 * 60 * 60)) % 24);
+    
+        return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+      };
 
     const diferenciaDias = (fecha) => {
         let fecha_actual = new Date();
@@ -193,6 +220,7 @@ const Casos_component = () => {
                                             <th className="font-normal px-3 pt-0 pb-3 border-b border-gray-200 dark:border-gray-800 sm:text-gray-400 text-white">Mesa</th>
                                             <th className="font-normal px-3 pt-0 pb-3 border-b border-gray-200 dark:border-gray-800 sm:text-gray-400 text-white">Contrato</th>
                                             <th className="font-normal px-3 pt-0 pb-3 border-b border-gray-200 dark:border-gray-800 sm:text-gray-400 text-white">Acto Procesal</th>
+                                            <th className="font-normal px-3 pt-0 pb-3 border-b border-gray-200 dark:border-gray-800 sm:text-gray-400 text-white">Tiempo</th>
                                             <th className="font-normal px-3 pt-0 pb-3 border-b border-gray-200 dark:border-gray-800 sm:text-gray-400 text-white">Culminado</th>
                                             <th className="font-normal px-3 pt-0 pb-3 border-b border-gray-200 dark:border-gray-800 sm:text-gray-400 text-white">Ultimo acceso</th>
 
@@ -233,6 +261,23 @@ const Casos_component = () => {
                                                             "
                                                             onClick={() => abrirModalActoProcesal(data.acto_procesal)}
                                                         >Abrir</button>
+                                                    </td>
+                                                    <td className="sm:p-3 py-2 px-1 border-b border-gray-200 dark:border-gray-800 md:table-cell ">
+                                                        <div className="flex items-center">
+                                                            <button className="text-green-500
+                                                                hover:text-green-700 focus:outline-none
+                                                                focus:ring-2 focus:ring-green-300 font-medium rounded-lg text-sm px-2 py-1 text-center dark:text-green-400 dark:hover:text-green-500 dark:focus:ring-green-800 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:z-10
+                                                                "
+                                                                onClick={() => {
+                                                                    setTime(0);
+                                                                    setIsRunning(true);
+                                                                    const modal = document.getElementById('modal-cronometro');
+                                                                    modal.classList.remove('hidden');
+                                                                    modal.setAttribute('aria-hidden', 'false');
+                                                                }}
+                                                            >Iniciar</button>                                                         
+                                                          
+                                                        </div>
                                                     </td>
                                                     <td className={data.culminado < 50 ? "sm:p-3 py-2 px-1 border-b border-gray-200 dark:border-gray-800 md:table-cell text-red-500" : "sm:p-3 py-2 px-1 border-b border-gray-200 dark:border-gray-800 md:table-cell text-green-500"}>{data.culminado} %</td>
 
@@ -333,6 +378,58 @@ const Casos_component = () => {
 
                                 <p className="text-gray-600 dark:text-gray-300">{actoProcesal}</p>
                             </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {/* modal cronometro  -- formatTime */}
+            <div id="modal-cronometro" data-modal-show="true" aria-hidden="true" className="hidden overflow-x-hidden overflow-y-auto fixed h-modal md:h-full top-0 left-0 right-0 md:inset-0 z-50 flex items-center justify-center">
+                <div className="relative w-full max-w-2xl px-4 h-full md:h-auto">
+                    <div className="bg-white rounded-lg shadow relative dark:bg-gray-700">
+                        <div className="flex items-start justify-between p-5 border-b rounded-t dark:border-gray-600">
+                            <h3 className="text-gray-900 text-xl lg:text-2xl font-semibold dark:text-white">
+                                Tiempo laborado en caso 
+                            </h3>
+                            <button type="button" className="text-gray-400 bg-gray-900 hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-red-400 dark:hover:text-white" data-modal-toggle="modal-cronometro" onClick={
+                                () => {
+                                    setTime(0);
+                                    setIsRunning(false);
+                                    const modal = document.getElementById('modal-cronometro');
+                                    modal.classList.add('hidden');
+                                    modal.setAttribute('aria-hidden', 'true');
+                                }
+                            }>
+                                <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+                            </button>
+                        </div>
+                        <div className="p-6  text-base">
+                                <h2 className='text-3xl font-bold text-center text-gray-900 dark:text-white'>{formatTime(time)}</h2>
+                                
+                                <p>Descripción</p>
+                                <textarea className='w-full h-24 px-3 py-2 text-base placeholder-gray-300 border border-gray-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-primary-450 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-100' placeholder='Agregar descripción (Opcional)'></textarea>          
+                                <div className='flex justify-between mt-5'>
+                                       <div className='space-x-2'>
+                                            <button className='bg-gray-500 hover:bg-gray-600 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center text-white dark:bg-gray-500 dark:hover:bg-gray-600 dark:focus:ring-gray-800'  onClick={() => setIsRunning(!isRunning)}>{isRunning ? 'Pausar' : 'Reanudar'}</button>
+                                            <button className='bg-secondary-700 hover:bg-secondary-600 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center text-white dark:bg-secondary-450 dark:hover:bg-secondary-600 dark:focus:ring-blue-800'  onClick={() => setTime(0)}>Reiniciar</button> 
+                                       </div>
+                                       
+                                
+                                       <div className='space-x-2'>
+                                            <button className='bg-primary-450 hover:bg-primary-600 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center text-white dark:bg-primary-450 dark:hover:bg-primary-600 dark:focus:ring-blue-800'>Guardar</button>
+                                            <button className='bg-gray-500 hover:bg-gray-600 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center text-white dark:bg-gray-500 dark:hover:bg-gray-600 dark:focus:ring-gray-800' onClick={
+                                                () => {
+                                                    setTime(0);
+                                                    setIsRunning(false);
+                                                    const modal = document.getElementById('modal-cronometro');
+                                                    modal.classList.add('hidden');
+                                                    modal.setAttribute('aria-hidden', 'true');
+                                                }
+                                            } >Cancelar</button>
+                                        </div>
+                                </div>
+                                
 
                         </div>
                     </div>
