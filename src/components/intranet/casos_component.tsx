8ec928d22bd667c4c3633a0b4b0f2a0565b81c55
FrankCairampoma <ingfralch@gmail.com>
2024-05-09 18:10:39 -0500

    Agrega funcionalidad de inicio de sesión con tecla Enter en el componente Login.tsx

--- src/components/intranet/casos_component.tsx
+++ src/components/intranet/casos_component.tsx
@@ -285,6 +285,8 @@ const Casos_component = () => {
                                                                 const modal = document.getElementById('modal-table-pagos');
                                                                 modal.classList.remove('hidden');
                                                                 modal.setAttribute('aria-hidden', 'false');
+                                                                setIsModalOpen(true);
+                                                                blurRef.current.classList.add('modal-open');
                                                             }}
                                                         >Ver</button>
                                                     </td>
@@ -484,7 +486,17 @@ const Casos_component = () => {
                             <h3 className="text-gray-900 text-xl lg:text-2xl font-semibold dark:text-white">
                                 Pagos
                             </h3>
-                            <button type="button" className="text-gray-400 bg-gray-900 hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-red-400 dark:hover:text-white" data-modal-toggle="modal-table-pagos">
+                            <button type="button" className="text-gray-400 bg-gray-900 hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-red-400 dark:hover:text-white" data-modal-toggle="modal-table-pagos" 
+                                onClick={
+                                    () => {
+                                        const modal = document.getElementById('modal-table-pagos');
+                                        modal.classList.add('hidden');
+                                        modal.setAttribute('aria-hidden', 'true');
+                                        setIsModalOpen(false);
+                                        blurRef.current.classList.remove('modal-open');
+                                    }
+                                }
+                            >
                                 <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
                             </button>
                         </div>
