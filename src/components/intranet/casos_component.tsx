e3391f28a4f26010a649614d04bd7618a1a39397
FrankCairampoma <ingfralch@gmail.com>
2024-05-13 18:16:26 -0500

    feat: improve file upload UI in casos_component.tsx

--- src/components/intranet/casos_component.tsx
+++ src/components/intranet/casos_component.tsx
@@ -599,6 +599,23 @@ const Casos_component = () => {
 
             <div id="modal-upload-file" data-modal-show="true" aria-hidden="true" className="hidden overflow-x-hidden overflow-y-auto fixed h-modal md:h-full top-0 left-0 right-0 md:inset-0 z-50 flex items-center justify-center">                 
                         <div className="sm:max-w-lg w-full p-10 bg-primary-980 rounded-xl z-10">
+                            {/* boton cerrar modal */}
+                            <div className="flex items-start justify-between">
+                                <h3 className="text-gray-900 text-xl lg:text-2xl font-semibold dark:text-white">
+                                    Subir Contrato
+                                </h3>
+                                <button type="button" className="text-gray-400 bg-gray-900 hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-red-400 dark:hover:text-white" data-modal-toggle="modal-upload-file" onClick={
+                                    () => {
+                                        const modal = document.getElementById('modal-upload-file');
+                                        modal.classList.add('hidden');
+                                        modal.setAttribute('aria-hidden', 'true');
+                                        setIsModalOpen(false);
+                                        blurRef.current.classList.remove('modal-open');
+                                    }
+                                }>
+                                    <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+                                </button>
+                            </div>
                             <div className="text-center">
                                 <h2 className="mt-5 text-3xl font-bold text-white">
                                     Contrato
