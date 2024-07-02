c3857b6f5b8dc39a58db409f75a689215043de1e
FrankCairampoma <ingfralch@gmail.com>
2024-07-02 17:55:52 -0500

    Actualizar URL de API en componentes de intranet

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
-import react from "@astrojs/react";
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react()],
+  headers: {
+    'Content-Security-Policy':
+      "frame-ancestors 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; connect-src 'self' http://162.248.55.24:3000;",
+  },
 });
